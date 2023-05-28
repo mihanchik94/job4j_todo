7ad0569ae84ea6ad5685d43d135e0f72939c51d2
mihanchik94 <mihanchik94@gmail.com>
2023-05-28 21:22:59 +0300

    Add model

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,15 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class Task {
+    private int id;
+    private String description;
+    private final LocalDateTime created = LocalDateTime.now();
+    private boolean done;
+}
