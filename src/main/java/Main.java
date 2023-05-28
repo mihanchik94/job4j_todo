adb81bdbfe14c7f24d2ddcb53236beadfec07c7e
mihanchik94 <mihanchik94@gmail.com>
2023-05-28 21:41:53 +0300

    Add hibernate configuration

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.context.annotation.Bean;
+
+public class Main {
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+}
