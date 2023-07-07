8c0370daa394bec5ac9e2823970bbe8264fc4910
mihanchik94 <mihanchik94@gmail.com>
2023-07-07 22:37:57 +0300

    Add update and delete task functions to controller, page with one task (with tests)

--- src/main/java/ru/job4j/todo/controller/TaskController.java
+++ src/main/java/ru/job4j/todo/controller/TaskController.java
@@ -4,12 +4,11 @@ import lombok.AllArgsConstructor;
 import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.todo.model.Task;
 import ru.job4j.todo.service.TaskService;
+
+import java.util.Optional;
 
 @ThreadSafe
 @Controller
@@ -51,5 +50,37 @@ public class TaskController {
             model.addAttribute("message", e.getMessage());
             return "errors/404";
         }
+    }
+
+    @GetMapping("{id}")
+    public String getById(Model model, @PathVariable int id) {
+        Optional<Task> taskOptional = taskService.findById(id);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "заявка с указанным id не найдена");
+            return "redirect:/errors/404";
+        }
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/one";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Task task, Model model) {
+        try {
+            taskService.update(task);
+            return "redirect:/tasks/all";
+        } catch (Exception e) {
+            model.addAttribute("message", e.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable int id, Model model) {
+        boolean isDeleted = taskService.deleteById(id);
+        if (!isDeleted) {
+            model.addAttribute("message", "Задача с указанным id не найдена");
+            return "errors/404";
+        }
+        return "redirect:/tasks/all";
     }
 }
